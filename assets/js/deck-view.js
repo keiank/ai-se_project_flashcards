8ee29050f13be347c81690065b2c16181142b5e1
keiank <278810033+keiank@users.noreply.github.com>
2026-05-25 20:37:38 -0400

    Feat: create new card in a deck

--- assets/js/deck-view.js
+++ assets/js/deck-view.js
@@ -1,7 +1,54 @@
-import { deleteCard } from "./api.js";
+import { createCard, deleteCard } from "./api.js";
 import { hexToString, removeColorClasses } from "./colorMap.js";
 import { confirmDeletion } from "./modal.js";
-import { removeCardById } from "./decks.js";
+import {
+  fetchedDecks,
+  getDeckByID,
+  getDeckIndex,
+  removeCardById,
+} from "./decks.js";
+
+const deckSection = document.querySelector("#deck-view");
+const galleryList = deckSection.querySelector(".gallery__list");
+
+function createNewCard(card, { deckId, deckHexColor }) {
+  const template = document.querySelector(".flashcard-template");
+  const cardEl = template.content.querySelector(".card").cloneNode(true);
+  removeColorClasses(cardEl);
+  const cardColor = hexToString(deckHexColor);
+  cardEl.classList.add(`card_color_${cardColor}`);
+
+  const cardTitle = cardEl.querySelector(".card__title");
+  cardTitle.textContent = card.question;
+
+  const flipBtn = cardEl.querySelector(".card__flip-btn");
+  let flipped = false;
+  flipBtn.addEventListener("click", () => {
+    flipped = !flipped;
+    cardTitle.textContent = flipped ? card.answer : card.question;
+    removeColorClasses(cardEl);
+    cardEl.classList.add(
+      flipped ? "card_color_white" : `card_color_${cardColor}`,
+    );
+  });
+
+  const deleteBtn = cardEl.querySelector(".card__delete-btn");
+  deleteBtn.onclick = () => {
+    confirmDeletion("card", () => {
+      deleteCard(card._id)
+        .then(() => {
+          // remove from UI
+          cardEl.remove();
+          // remove from browser storage - so it doesn't reload when navigating back
+          // to deck view
+          removeCardById(deckId, card._id);
+        })
+        .catch((err) => console.error(`Error: unable to delete card ${err}`));
+    });
+  };
+
+  return cardEl;
+}
 
 /**
  * Renders the deck view for a given deck object.
@@ -10,53 +57,75 @@ import { removeCardById } from "./decks.js";
  * @param {string} deck.color - The hex color code for the deck
  * @param {Array<Object>} deck.cards - Array of card objects with question and answer properties
  */
-export function renderDeckView(deck) {
-  const page = document.querySelector(".page");
-  const deckSection = document.querySelector("#deck-view");
-  const main = document.querySelector("main");
-
-  const carouselStyle = "page__main-content_type_carousel";
-  main.classList.remove(carouselStyle);
-
-  page.classList.remove("page_no-mobile-bar");
-
+function renderDeckView(deck) {
   const title = deckSection.querySelector(".gallery__title");
   title.textContent = deck.name;
-
-  const galleryList = deckSection.querySelector(".gallery__list");
   galleryList.textContent = "";
-  const template = document.querySelector(".flashcard-template");
-  const cardColor = hexToString(deck.color);
+
   deck.cards.forEach((card) => {
-    const cardEl = template.content.querySelector(".card").cloneNode(true);
-    removeColorClasses(cardEl);
-    cardEl.classList.add(`card_color_${cardColor}`);
-    const cardTitle = cardEl.querySelector(".card__title");
-    cardTitle.textContent = card.question;
-
-    const flipBtn = cardEl.querySelector(".card__flip-btn");
-    let flipped = false;
-    flipBtn.onclick = () => {
-      flipped = !flipped;
-      cardTitle.textContent = flipped ? card.answer : card.question;
-      removeColorClasses(cardEl);
-      cardEl.classList.add(
-        flipped ? "card_color_white" : `card_color_${cardColor}`,
-      );
-    };
-
-    const deleteBtn = cardEl.querySelector(".card__delete-btn");
-    deleteBtn.onclick = () => {
-      confirmDeletion("card", () => {
-        deleteCard(card._id)
-          .then(() => {
-            cardEl.remove();
-            removeCardById(deck._id, card._id);
-          })
-          .catch((err) => console.error(`Error: unable to delete card ${err}`));
-      });
-    };
-
-    galleryList.prepend(cardEl);
+    const newCardEl = createNewCard(card, {
+      deckId: deck._id,
+      deckHexColor: deck.color,
+    });
+    galleryList.prepend(newCardEl);
   });
 }
+
+function swapCardColor(cardEl, deckColor, questionDisplayed) {
+  removeColorClasses(cardEl);
+  if (questionDisplayed) {
+    cardEl.classList.add(`card_color_white`);
+  } else {
+    cardEl.classList.add(`card_color_${deckColor}`);
+  }
+  return !questionDisplayed;
+}
+
+function showNewCard(deck) {
+  const newCardTemplate = document.querySelector("#new-card-template");
+  const newCardEl = newCardTemplate.content
+    .querySelector(".new-card-template__form")
+    .cloneNode(true);
+  // Initial side should be question side - deck color, question input field shown
+  const cardColor = hexToString(deck.color);
+  const answer = newCardEl.querySelector(".new-card-template__answer");
+  const question = newCardEl.querySelector(".new-card-template__question");
+  const visibleClass = "new-card-template_side_visible";
+  newCardEl.classList.add(`card_color_${cardColor}`);
+  question.classList.toggle(visibleClass);
+  let isQuestionSide = true;
+
+  const flipBtn = newCardEl.querySelector(".card__flip-btn");
+  flipBtn.addEventListener("click", () => {
+    // Show the other input field
+    answer.classList.toggle(visibleClass);
+    question.classList.toggle(visibleClass);
+    // Toggle color between deck color and white
+    isQuestionSide = swapCardColor(newCardEl, cardColor, isQuestionSide);
+  });
+
+  const createBtn = newCardEl.querySelector(".card__create-btn");
+  createBtn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    createCard(deck._id, {
+      question: question.value,
+      answer: answer.value,
+    }).then((card) => {
+      const index = getDeckIndex(deck._id);
+      // add to browser storage
+      fetchedDecks[index].cards.push(card);
+      // add deck to list of cards without reloading page
+      const cardEl = createNewCard(card, {
+        deckId: deck._id,
+        deckHexColor: deck.color,
+      });
+      // remove new card button from UI
+      // and replace it with the card we committed to the DB
+      newCardEl.replaceWith(cardEl);
+    });
+  });
+
+  galleryList.append(newCardEl);
+}
+
+export { renderDeckView, showNewCard };
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 import { fetchedDecks, getDeckByID } from "./decks.js";
 import { hexToString, removeColorClasses } from "./colorMap.js";
 import { renderCarouselView } from "./carousel.js";
-import { renderDeckView } from "./deck-view.js";
+import { renderDeckView, showNewCard } from "./deck-view.js";
 import { confirmDeletion } from "./modal.js";
 import { disableSubmitBtn } from "./new-deck-view.js";
 import { getDecks, deleteDeck, removeDeckByID } from "./api.js";
@@ -118,6 +118,13 @@ const homeNewDeckBtn = homeSection.querySelector(".gallery__new-card-btn");
 homeNewDeckBtn.onclick = () => {
   window.location.hash = "#new-deck-view";
 };
+
+const deckViewNewCardBtn = deckSection.querySelector(
+  ".gallery__new-card-btn_location_deck-view",
+);
+deckViewNewCardBtn.addEventListener("click", () => {
+  showNewCard(currentDeck);
+});
 
 /**
  * Routes to the appropriate view based on the current URL hash.
